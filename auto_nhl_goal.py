0ee15e27de27702e622bf3ed80df478fda016c16
arim215 <ari.mandjelikian@gmail.com>
2016-03-21 18:41:57 -0400

    Updated code for sleep during off season and in the morning.

--- auto_nhl_goal.py
+++ auto_nhl_goal.py
@@ -2,7 +2,7 @@
 
 #test
 
-import time, os, random
+import time, datetie, os, random
 import RPi.GPIO as GPIO
 import subprocess, ctypes
 
@@ -34,51 +34,65 @@ def fetch_score():
 	return score
 
 def score_int(score)
-    try:
-	    score=int(score)
-    except:
-	    pass
-    return score
+    	try:
+		score=int(score)
+    	except:
+	    	pass
+    	return score
 
+def check_season():
+	now=datetime.now()
+	while now.month in (7, 8, 9):
+            if now.day < 30 and now.month < 9:
+		time.sleep(8640)
+		now = datetime.datetime.now()
+	while now.hour < 11:
+		if now.minute < 30 :
+            		time.sleep(900)
+            		now = datetime.datetime.now()
+
+
+#MAIN
+
+#init        	
 old_score=0
 new_score=0
-
 old_score=fetch_score()
-
 old_score=score_int(old_score)
 
 
 print ("When a goal is scored, please press the GOAL button...")
 try:
-while (1):
-
-	#check the state of the button/site two times per second
-	time.sleep(0.5)
+	while (1):
 	
-	#Check score online and save score
-	new_score=fetch_score()
-	#Convert string to integer
-    
-	new_score=score_int(new_score)	
-
-
-	#If new game, replace old score with 0
-	if old_score > new_score:
-		old_score=0
+		check_season()
+		#check the state of the button/site two times per second
+		time.sleep(0.5)
 		
-	#If score change...
-	if new_score > old_score:
-		#save new score
-		old_score=new_score
-		activate_goal_light()
-
-	#If the button is pressed
-	if(GPIO.input(15)==0):
-		#save new score
-		old_score=new_score
-		activate_goal_light()
+		#Check score online and save score
+		new_score=fetch_score()
+		#Convert string to integer
+	    
+		new_score=score_int(new_score)	
+	
+	
+		#If new game, replace old score with 0
+		if old_score > new_score:
+			old_score=0
+			
+		#If score change...
+		if new_score > old_score:
+			#save new score
+			old_score=new_score
+			activate_goal_light()
+	
+		#If the button is pressed
+		if(GPIO.input(15)==0):
+			#save new score
+			old_score=new_score
+			activate_goal_light()
 
 except KeyboardInterrupt:					
-#Restore GPIO to default state
-GPIO.cleanup()  
-print ("GPIO cleaned")
+	#Restore GPIO to default state
+	GPIO.cleanup()
+	print ("GPIO cleaned")
