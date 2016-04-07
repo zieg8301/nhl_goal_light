f60eda2e2bb1ec5009d15f929224994e66ce94cd
arim215 <ari.mandjelikian@gmail.com>
2016-04-07 15:56:05 -0400

    Update nhl_goal_light.py

--- nhl_goal_light.py
+++ nhl_goal_light.py
@@ -28,7 +28,7 @@ def get_team():
 	team_list=requests.get(url)
 	team_list=team_list.text[team_list.text.find(team)-50:team_list.text.find(team)]
 	print team_list
-	teamID=team_list.text[team_list.text.find("id")+6:team_list.text.find("id")+8]
+	teamID=team_list[team_list.find("id")+6:team_list.find("id")+8]
 	print teamID
 	return teamID
 
