dc5c4e8a140987152366e587eded691d72f2b89e
Ari Mandjelikian <arim215@users.noreply.github.com>
2016-09-06 15:43:10 -0400

    Update gpio_mock.py

--- lib/gpio_mock.py
+++ lib/gpio_mock.py
@@ -9,23 +9,23 @@ OUT = "OUTPUT"
 
 
 def setmode(mode):
-    print("Set mode {0!s}".format(mode))
+    print("Set mode {0}".format(mode))
 
 
 def setwarnings(mode):
-    print("Set warnings as {0!s}".format(mode))
+    print("Set warnings as {0}".format(mode))
 
 
 def setup(pin, mode):
-    print("Set pin {0!s} as {1!s}".format(pin, mode))
+    print("Set pin {0} as {1}".format(pin, mode))
 
 
 def output(pin, value):
-    print("Output {0!s} to pin {1!s}".format(value, pin))
+    print("Output {0} to pin {1}".format(value, pin))
 
 
 def input(pin):
-    print("Input 0 to pin {0!s}".format(s))
+    print("Input 0 to pin {0}".format(s))
     return 0
 
 
