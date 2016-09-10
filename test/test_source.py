da0da7acd264f2821048f15bf3506e1b68fae9f4
Ari Mandjelikian <arim215@users.noreply.github.com>
2016-09-09 21:42:56 -0400

    Update test_source.py

--- test/test_source.py
+++ test/test_source.py
@@ -61,7 +61,7 @@ def fetch_score2(team):
     game_id = game_id[game_id.find("id") + 4:]
 
     season_id = game_id[:4] + str(int(game_id[:4]) + 1)
-    url = "http://live.nhle.com/GameData/{0!s}/{1!s}/gc/gcbx.jsonp".format(
+    url = "http://live.nhle.com/GameData/{0}/{1}/gc/gcbx.jsonp".format(
         season_id, game_id)
     try:
         score = requests.get(url)
