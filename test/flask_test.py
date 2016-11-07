f5e5e1ae04452cbbacc28d01759c1232bb98a4a4
arim215 <ari.mandjelikian@gmail.com>
2016-11-07 18:34:45 -0500

    flask tests

--- test/flask_test.py
+++ test/flask_test.py
@@ -1,17 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
 def sel_team():
-    return """<html>
-                <body>
-                    Choose team :
-                    <select>
-                        <option value="canadiens">Canadiens</option>
-                        <option value="Bruins">Bruins</option>
-                    </select>
-                </body>
-            </html>"""
+    return render_template('setup.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
