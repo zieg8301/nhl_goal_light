ce0e8a96baaddb791b37956abd13a875bb18353a
Ari Mandjelikian <arim215@users.noreply.github.com>
2016-11-07 16:09:55 -0500

    Update flask_test.py

--- test/flask_test.py
+++ test/flask_test.py
@@ -2,10 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/options')
 def sel_team():
     return """<html>
                 <body>
