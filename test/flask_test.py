18b2c29b61b725df04764a789c10536dc31f186b
Ari Mandjelikian <arim215@users.noreply.github.com>
2016-11-08 10:09:16 -0500

    Update flask_test.py

--- test/flask_test.py
+++ test/flask_test.py
@@ -6,14 +6,14 @@ def sel_team():
     return render_template('setup.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
-def result():
+def result(time):
 
     if request.method == 'POST':
 
       team = request.form['Team']
       delay = request.form['Delay']
 
-      result = { 'team' : team, 'delay' : delay }
+      result = { 'team' : team, 'delay' : delay , 'time' : time}
 
       print("Result : {}".format(result))
 
@@ -22,3 +22,4 @@ def result():
 if __name__ == '__main__':
 
     app.run(host= '0.0.0.0', debug=True)    
+    time = 20
