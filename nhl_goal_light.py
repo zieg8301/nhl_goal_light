4dee2da2ab6c3b936fea7f965140c28d7c13b7e9
ari.mandjelikian <ari.mandjelikian@gmail.com>
2017-01-11 10:50:31 -0500

    fixed error of no settings.txt file found

--- nhl_goal_light.py
+++ nhl_goal_light.py
@@ -44,48 +44,54 @@ def sleep(sleep_period):
 def setup_nhl():
     """Function to setup the nhl_goal_light.py with team, team_id and API_URL"""
 
+    lines = ""
+    """
+    Try to find a settings.txt file in the folder to automaticly setup the goal light with pre-desired team, delay and API_URL.
+    settings.txt file should as such : Enter API URL, team_id, delay, each on a separate line in this order. LEAVE EMPTY if you want to manually input every time.
+    If program can't find settings.txt file or if file is empty, it will ask for user input.
+    """
     if os.path.exists('./settings.txt'):
         # get settings from file
         f = open('settings.txt', 'r')
         lines = f.readlines()
         
-        #find API_URL      
-        try :
-            API_URL = lines[1].strip('\n')
-        except IndexError :
-            API_URL = ""
+    #find API_URL      
+    try :
+        API_URL = lines[1].strip('\n')
+    except IndexError :
+        API_URL = ""
+    if API_URL == "":
+        API_URL = input("Enter Flask API IP or URL. (If empty, default will be localhost) \n")
         if API_URL == "":
-            API_URL = input("Enter Flask API IP or URL. (If empty, default will be localhost) \n")
-            if API_URL == "":
-                API_URL = "http://localhost:8080/api/v1/"
-            else:
-                API_URL = "http://" + API_URL + ":8080/api/v1/"
-        
-        #find team_id
-        try :
-            team_id = lines[2].strip('\n')
-        except IndexError :
-            team_id = ""
-        if team_id == "":
-            team = input("Enter team you want to setup (without city) (Default: Canadiens) \n")
-            if team == "":
-                team = "Canadiens"
-            else:
-                team = team.title()
-        # query the api to get the ID
-        response = requests.get("{}team/{}/id".format(API_URL, team))
-        team_id = response.json()['id']
-        
-        #find delay
-        try:
-            delay = lines[3].strip('\n')
-        except IndexError :
-            delay = ""
+            API_URL = "http://localhost:8080/api/v1/"
+        else:
+            API_URL = "http://" + API_URL + ":8080/api/v1/"
+
+    #find team_id
+    try :
+        team_id = lines[2].strip('\n')
+    except IndexError :
+        team_id = ""
+    if team_id == "":
+        team = input("Enter team you want to setup (without city) (Default: Canadiens) \n")
+        if team == "":
+            team = "Canadiens"
+        else:
+            team = team.title()
+    # query the api to get the ID
+    response = requests.get("{}team/{}/id".format(API_URL, team))
+    team_id = response.json()['id']
+
+    #find delay
+    try:
+        delay = lines[3].strip('\n')
+    except IndexError :
+        delay = ""
+    if delay is "":
+        delay = input("Enter delay required to sync : \n")
         if delay is "":
-            delay = input("Enter delay required to sync : \n")
-            if delay is "":
-                delay = 0
-        delay = float(delay)
+            delay = 0
+    delay = float(delay)
     print (team_id, API_URL, delay)
     return (team_id, API_URL, delay)
 
