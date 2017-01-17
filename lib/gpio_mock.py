3f085d921c27bc5dee6cba3e91ca3e23795d1c51
Ari Mandjelikian <arim215@users.noreply.github.com>
2017-01-17 12:02:47 -0500

    Update gpio_mock.py

--- lib/gpio_mock.py
+++ lib/gpio_mock.py
@@ -16,15 +16,10 @@ def setwarnings(mode):
     print("Set warnings as {0}".format(mode))
 
 
-<<<<<<< HEAD
 def setup(pin, mode, pull =""):
     print("Set pin {0} as {1} {2}".format(pin, mode, pull))
-=======
-def setup(pin, mode,pull=""):
-    print("Set pin {0} as {1} {3}".format(pin, mode, pull))
->>>>>>> a6739ab2896836db58e44782576083f9404ddd9e
 
-
+    
 def output(pin, value):
     print("Output {0} to pin {1}".format(value, pin))
 
