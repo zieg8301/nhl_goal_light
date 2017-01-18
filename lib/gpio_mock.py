1825fe25f3440b913b6854634e18d99eda3dd7f2
Ari Mandjelikian <arim215@users.noreply.github.com>
2017-01-18 14:07:01 -0500

    Update gpio_mock.py

--- lib/gpio_mock.py
+++ lib/gpio_mock.py
@@ -8,6 +8,8 @@ IN = "INPUT"
 OUT = "OUTPUT"
 PUD_DOWN = "PULL_DOWN"
 PUD_UP = "PULL_UP"
+RISING = "RISING"
+FALLING = "FALLING"
 
 def setmode(mode):
     print("Set mode {0}".format(mode))
@@ -17,10 +19,10 @@ def setwarnings(mode):
     print("Set warnings as {0}".format(mode))
 
 
-def setup(pin, mode, pull =""):
+def setup(pin, mode, pull=" "):
     print("Set pin {0} as {1} {2}".format(pin, mode, pull))
 
-    
+
 def output(pin, value):
     print("Output {0} to pin {1}".format(value, pin))
 
@@ -29,6 +31,14 @@ def input(pin):
     print("Input 0 to pin {0}".format(pin))
     return 0
 
+  
+def add_event_detect(pin, mode, func, bouncetime):
+    print("pin {0}, mode {1}, func {2}, bouncetime {3}".format(pin,mode,func,bouncetime) )
+    
 
+def remove_event_detect(pin):
+    print("event detection on pin {0} removed".format(pin))    
+    
+    
 def cleanup():
     print("Cleanup done")
